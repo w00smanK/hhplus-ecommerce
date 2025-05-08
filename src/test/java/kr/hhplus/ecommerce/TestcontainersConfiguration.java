16979edc4ec13419d0742a10bf9f82a272448825
wookyoungkim <pd1125@naver.com>
2025-05-08 23:05:12 +0900

    feat : redis연동 테스트 / redisson 설정

--- src/test/java/kr/hhplus/ecommerce/TestcontainersConfiguration.java
+++ src/test/java/kr/hhplus/ecommerce/TestcontainersConfiguration.java
@@ -2,6 +2,7 @@ package kr.hhplus.ecommerce;
 
 import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -9,6 +10,7 @@ import org.testcontainers.utility.DockerImageName;
 class TestcontainersConfiguration {
     //test
     public static final MySQLContainer<?> MYSQL_CONTAINER;
+    public static final GenericContainer<?> REDIS_CONTAINER;
 
     static {
         MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
@@ -20,12 +22,21 @@ class TestcontainersConfiguration {
         System.setProperty("spring.datasource.url", MYSQL_CONTAINER.getJdbcUrl() + "?characterEncoding=UTF-8&serverTimezone=UTC");
         System.setProperty("spring.datasource.username", MYSQL_CONTAINER.getUsername());
         System.setProperty("spring.datasource.password", MYSQL_CONTAINER.getPassword());
+
+        REDIS_CONTAINER = new GenericContainer<>(DockerImageName.parse("redis:7.2"))
+                .withExposedPorts(6379);
+        REDIS_CONTAINER.start();
+        System.setProperty("spring.data.redis.host", REDIS_CONTAINER.getHost());
+        System.setProperty("spring.data.redis.port", String.valueOf(REDIS_CONTAINER.getMappedPort(6379)));
     }
 
     @PreDestroy
     public void preDestroy() {
         if (MYSQL_CONTAINER.isRunning()) {
             MYSQL_CONTAINER.stop();
+        }
+        if (REDIS_CONTAINER.isRunning()) {
+            REDIS_CONTAINER.stop();
         }
     }
 }
